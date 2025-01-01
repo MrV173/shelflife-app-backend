c52c419b9213266aee92e45ae3df613bf368f632
MrV173 <ibnuhakim81@gmail.com>
2025-01-01 20:22:36 +0700

    change config database

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  "shelfli1_db_shelflife",
-  "shelfli1_admin",
-  ",55ID^5bjmr~",
-  {
-    host: "localhost",
-    dialect: "mysql",
-  }
-);
+const db = new Sequelize("tester", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export default db;
--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -3,29 +3,23 @@ import db from "../config/Database.js";
 
 const { DataTypes } = Sequelize;
 
-const Category = db.define(
-  "categories",
-  {
-    uuid: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    name: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
+const Category = db.define("categories", {
+  uuid: {
+    type: DataTypes.STRING,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
   },
-  {
-    freezeTableName: true,
-  }
-);
+  name: {
+    type: DataTypes.STRING,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
 
 export default Category;
